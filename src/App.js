e3954a8b99ce9dcfa0eac513a43957898dcd125b
Tathagat Bhanj Dev <tathagatbhanjdev@gmail.com>
2022-12-21 00:55:21 +0530

    Commit 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import React ,{useState} from 'react';
 function App() {
 
+  const [mode,setMode]=useState('light');
+   const toggleMode=() =>{
+    if(mode== 'light'){
+    setMode('dark');
+    }
+    else{
+    setMode('light');
+    } 
+  }
   return (
     < >
     
-    <Navbar title="TextUtils"  abouttext="About TextUtils"/>
+    <Navbar title="TextUtils"  abouttext="About TextUtils" mode={mode} toggleMode={toggleMode} />
     <div className="container">
     < TextForm  heading="Enter Text to analyze"/>
   </div>
