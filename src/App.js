8234372b10742cd688df7c88f9559722233aba28
Tathagat Bhanj Dev <tathagatbhanjdev@gmail.com>
2022-12-21 23:35:31 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import React ,{useState} from 'react';
+import About from "./components/About";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+
 function App() {
 
   const [mode,setMode]=useState('light');
@@ -14,12 +22,27 @@ function App() {
   }
   return (
     < >
-    
-    <Navbar title="TextUtils"  abouttext="About TextUtils" mode={mode} toggleMode={toggleMode} />
-    <div className="container">
-    < TextForm  heading="Enter Text to analyze"/>
-  </div>
-  </>
+     <Router>
+      <Navbar title = "Text Utils" abouttext="About" mode = {mode} toggleMode={toggleMode}/>
+      <div className="container">
+      </div>
+        <Routes>
+          <Route path='/about' element = {
+            <div className="container">
+              <About/>
+            </div>}>
+          </Route>
+
+          <Route path='/' element={
+            <div className="container">
+              <TextForm heading ="Enter the text to analyse" mode = {mode}/> 
+            </div>}>
+          </Route>
+        </Routes>
+    </Router>
+    </>
+  
+  
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+export default function 
+() {
+  return (
+    <div className="container">
+        <div className="accordion" id="accordionExample">
+  <div className="accordion-item">
+    <h2 className="accordion-header" id="headingOne">
+      <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+         About #1
+      </button>
+    </h2>
+    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div className="accordion-body">
+        <strong>This is the our Text Analyzer Web Application</strong> 
+      </div>
+    </div>
+  </div>
+  <div className="accordion-item">
+    <h2 className="accordion-header" id="headingTwo">
+      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        About #2
+      </button>
+    </h2>
+    <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+      <div className="accordion-body">
+        <strong>It Converts your text to lower and upper case</strong> 
+      </div>
+    </div>
+  </div>
+  <div className="accordion-item">
+    <h2 className="accordion-header" id="headingThree">
+      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        About  #3
+      </button>
+    </h2>
+    <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+      <div className="accordion-body">
+        <strong>It can read your Text!!.</strong>
+      </div>
+    </div>
+  </div>
+</div>
+        
+
+
+    </div>
+  )
+}
